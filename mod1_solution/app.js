ac5af959947f4cad9df9d700e1ce4fd7f7b6b22d
samuelGweir <samuelGweir@gmail.com>
2022-01-09 14:39:03 -0500

    added comments

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -4,7 +4,7 @@
 angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
-
+//empty item is not considered as an item in list
 function LunchCheckController($scope){
    $scope.placeHolder = "list comma separated dishes you usually have for lunch";
    $scope.getMessage = function (){
